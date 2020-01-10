7799a97980058a213bd97d292dc19ce9952fc19f
Matt Milton <mattmilton57@gmail.com>
2020-01-10 05:25:30 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,15 +4,18 @@ p sum
 end
 
 def subtraction(num1, num2)
-
+tract = num1-num2
+p tract
 end
 
 def division(num1, num2)
-
+div = num1/num2
+p div
 end
 
 def multiplication(num1, num2)
-
+mult = num1*num2
+p mult
 end
 
 def modulo(num1, num2)
