ac24f03b525486a379f18a1d7687440a600fd409
Matt Milton <mattmilton57@gmail.com>
2020-01-10 06:01:32 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,31 @@
 def addition(num1, num2)
-
+sum = num1+num2
+p sum
 end
 
 def subtraction(num1, num2)
-
+tract = num1-num2
+p tract
 end
 
 def division(num1, num2)
-
+div = num1/num2
+p div
 end
 
 def multiplication(num1, num2)
-
+mult = num1*num2
+p mult
 end
 
 def modulo(num1, num2)
-
+modrod = num1/num2
+ans = modrod*num2 
+ans2 = num1-ans
+p ans2
 end
 
 def square_root(num)
-
+sqr = Math.sqrt(num)
+p sqr
 end
